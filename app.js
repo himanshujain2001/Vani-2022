d0d70b9b105843648a3a52c1acc7745cd22792ce
himanshujain <himanshujain@gmail.com>
2023-07-20 17:47:15 +0530

    Adding port

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.post("/login",function(req,res){
   }
 });
 
-let port = process.env.PORT;
+let port = process.env.PORT || 3000;
 
 app.listen(port,function(){
   console.log("Server is running on port "+port);
