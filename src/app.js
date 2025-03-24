a9db2c45a574f323ef8241657eb17cdc09e7a147
Himanshu Jain <himanshu_jain@epam.com>
2025-03-24 17:30:59 +0530

    rate limiting added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cookieParser from "cookie-parser"
+import rateLimit from "express-rate-limit"
 
 const app = express();
 
@@ -10,6 +11,12 @@ app.use(express.static("public")) // to get data from static files
 app.use(express.urlencoded({extended: true})) // to get data from url
 app.use(cookieParser()) // to store and remove cookies in user's browser
 
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10, // Limit each IP to 10 requests
+    message: "Server Error: Too many requests, please try again after sometime",
+  });
+
 app.set('view engine', 'ejs');
 
 // import router
@@ -17,7 +24,7 @@ app.set('view engine', 'ejs');
 import userRoute from "./routes/user.route.js"
 
 // router defination
-app.use('/vani-2022/users', userRoute)
+app.use('/vani-2022/users', apiLimiter, userRoute)
 
 // only telling render to do naviagte to this path as it's not going automatically to this for homepage
 // Redirect all requests from the root ("/") to the base path
